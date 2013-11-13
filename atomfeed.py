19cce96ffa98f29b7e420113d700f77d995339de
Scott Banwart <sbanwart@rogue-technology.com>
2013-11-12 21:10:20 -0500

    Updated all scripts to Python 3

--- atomfeed.py
+++ atomfeed.py
@@ -1,4 +1,4 @@
-from urlparse import urljoin
+from urllib.parse import urljoin
 from flask import Flask, request
 from werkzeug.contrib.atom import AtomFeed
 from time import gmtime, strftime
@@ -12,8 +12,8 @@ def make_external(url):
 @app.route('/moops.atom')
 def recent_feed():
     feed = AtomFeed('Orders', feed_url=request.url, url=request.url_root)
-    feed.add("Order", unicode("<order><id>1000</id><customer>1023</customer><total>$20.94</total></order>"), content_type="xml", author="edi", url=make_external("orders/1000"), updated=datetime.utcnow(), published=datetime.utcnow())
-    feed.add("Order", unicode("<order><id>1001</id><customer>2034</customer><total>$443.76</total></order>"), content_type="xml", author="edi", url=make_external("orders/1001"), updated=datetime.utcnow(), published=datetime.utcnow())
+    feed.add("Order", "<order><id>1000</id><customer>1023</customer><total>$20.94</total></order>", content_type="xml", author="edi", url=make_external("orders/1000"), updated=datetime.utcnow(), published=datetime.utcnow())
+    feed.add("Order", "<order><id>1001</id><customer>2034</customer><total>$443.76</total></order>", content_type="xml", author="edi", url=make_external("orders/1001"), updated=datetime.utcnow(), published=datetime.utcnow())
     return feed.get_response()
 
 if __name__ == "__main__":
--- file_to_atom.py
+++ file_to_atom.py
@@ -1,6 +1,6 @@
 #from urlparse import urljoin
 from urllib.parse import urljoin
-from flask import Flask, request
+from flask import Flask, request, render_template_string, make_response
 from werkzeug.contrib.atom import AtomFeed
 from time import gmtime, strftime
 from datetime import datetime
@@ -14,8 +14,9 @@ app = Flask(__name__)
 def make_external(url):
     return urljoin(request.url_root, url)
 
-@app.route('/orders.atom')
+@app.route('/orders.atom', methods=['GET'])
 def recent_feed():
+    print('Getting orders ATOM feed')
     feed = AtomFeed('Orders', feed_url=request.url, url=request.url_root)
     for fname in files:
         urlid = str(uuid1())
@@ -28,6 +29,11 @@ def recent_feed():
                         updated=datetime.utcnow(),
                         published=datetime.utcnow())
     return feed.get_response()
+
+@app.route('/orders', methods=['POST'])
+def create_order():
+    print('Order data: {}'.format(request.data))
+    return make_response(render_template_string('Done'), 201)
 
 if __name__ == "__main__":
     app.run(debug=True)
